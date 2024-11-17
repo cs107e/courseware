d47bc1a6b6ea626b4456d8ea92886305504b1bd3
Julie Zelenski <zelenski@users.noreply.github.com>
2024-11-17 06:16:13 +0000

    wip sensor lecture commit 6c666d9a6941fbba922c9710cedd66d7a3b0729b Author: Julie Zelenski <zelenski@users.noreply.github.com> Date:   Sat Nov 16 22:13:14 2024 -0800
        wip sensor lecture

--- cs107e/include/rand.h
+++ cs107e/include/rand.h
@@ -4,7 +4,6 @@
 /*
  * Module to generate random numbers.
  *
- * Author: Philip Levis <pal@cs.stanford.edu>
  * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
  */
 
@@ -12,10 +11,19 @@
  * `rand`
  *
  * Generate a 32-bit random number. Uses a psuedo-random
- * generator with a fixed seed.
+ * generator with a fixed seed by default.
  *
  * @return   pseudo-random value in the range 0 to UINT_MAX
  */
 unsigned int rand(void);
+
+/*
+ * `srand`
+ *
+ * Use argument as seed for sequence of pseudo-random numbers to be
+ * returned by rand().  Sequence is repeatable by calling srand()
+ * with the same seed value.
+ */
+void srand(unsigned int seed);
 
 #endif
--- cs107e/src/rand.c
+++ cs107e/src/rand.c
@@ -1,24 +1,47 @@
 /*
  * Pseudorandom number generator
  *
- * Author: Philip Levis <pal@cs.stanford.edu>
  * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
+ * Author: Julie Zelenski <zelenski@cs.stanford.edu>
  */
 
 #include "rand.h"
+#include <stdbool.h>
+#include <stdint.h>
 
-// From http://stackoverflow.com/questions/1167253/implementation-of-rand
+static struct {
+    bool initialized;
+    uint32_t z1, z2, z3, z4;
+} module = {
+    .initialized = false,
+};
+
 unsigned int rand(void) {
-    static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
-    unsigned int b;
+    if (!module.initialized) {
+        srand(12345);
+    }
+    // http://stackoverflow.com/questions/1167253/implementation-of-rand
+    // LFSR113 from L'écuyer
+    uint32_t b;
+    b = ((module.z1 << 6) ^ module.z1) >> 13;
+    module.z1 = ((module.z1 & 4294967294U) << 18) ^ b;
+    b = ((module.z2 << 2) ^ module.z2) >> 27;
+    module.z2 = ((module.z2 & 4294967288U) << 2) ^ b;
+    b = ((module.z3 << 13) ^ module.z3) >> 21;
+    module.z3 = ((module.z3 & 4294967280U) << 7) ^ b;
+    b = ((module.z4 << 3) ^ module.z4) >> 12;
+    module.z4 = ((module.z4 & 4294967168U) << 13) ^ b;
+    return (module.z1 ^ module.z2 ^ module.z3 ^ module.z4);
+}
 
-    b  = ((z1 << 6) ^ z1) >> 13;
-    z1 = ((z1 & 4294967294U) << 18) ^ b;
-    b  = ((z2 << 2) ^ z2) >> 27;
-    z2 = ((z2 & 4294967288U) << 2) ^ b;
-    b  = ((z3 << 13) ^ z3) >> 21;
-    z3 = ((z3 & 4294967280U) << 7) ^ b;
-    b  = ((z4 << 3) ^ z4) >> 12;
-    z4 = ((z4 & 4294967168U) << 13) ^ b;
-    return (z1 ^ z2 ^ z3 ^ z4);
+void srand(unsigned int seed) {
+    // initial factors must be >= 2, 8, 16, and 128 respectively
+    // this just lifts all to at least 128 to satisfy requirement
+    seed <<= 1;
+    if (seed < 128) seed += 128;
+    module.z1 = seed;
+    module.z2 = seed;
+    module.z3 = seed;
+    module.z4 = seed;
+    module.initialized = true;
 }
